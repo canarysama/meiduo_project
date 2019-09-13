44cf81c749f603fd4eb9d882573cd07d5757f423
xwp <xwp_fullstack@163.com>
2019-09-13 16:11:43 +0800

    1.新建配置文件

--- meiduo_mall/meiduo_mall/settings/prod.py
+++ meiduo_mall/meiduo_mall/settings/prod.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k%_sb^1v&y)+swl(b1i9p++o*l9#$_320iwq0i3)mjr*x8yrrm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
